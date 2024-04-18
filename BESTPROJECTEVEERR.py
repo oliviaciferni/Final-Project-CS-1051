5ef2b92b2948b9cd1d30732a3e48eeaaa06407fb
sheilaj03 <165831468+sheilaj03@users.noreply.github.com>
2024-04-18 15:33:16 -0400

    Update BESTPROJECTEVEERR.py

--- BESTPROJECTEVEERR.py
+++ BESTPROJECTEVEERR.py
@@ -1,2 +1,77 @@
-print("hello:)
-      
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("LightSteelBlue")
+wn.title("Escape 1051")
+wn.setup(700,700)
+
+#The pen for the blocks!
+class Pen(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.speed(0)
+
+#Create levels list
+levels = [""]
+
+#Define first level
+level_1 = [
+"XXXXXXXXXXXXXXXXXXXXXXXXX",
+"XP         XXXXXXXXXXXXXX",
+"X XXXXXXXX XXXXXXX     XX",
+"X XXXXXXXX XXXXXXX XXXXXX",
+"X XXXXXXXX XXXXXXX XXXXXX",
+"X XXXXXXXX XXXXXXX XXXXXX",
+"X     XXXX XXXXXXX XXXXXX",
+"XXXXX XXX               X",
+"XXXXX XXX XXXXXXXXXXXXX X",
+"XXX            XXXXXXXX X",
+"XXX XXXXXXXXXX XXXXXXXX X",
+"XXX XXXXXXXXXX XXXXXXXX X",
+"X   XXXXX               X",
+"X XXXXXXX XXXXXXXXXX XXXX",
+"X XXXXXXX XXXXXXXXXX XXXX",
+"X         XXXXX         X",
+"XXXXX XXXXXXXXX XXXX XXXX",
+"XXXXX XXXXXXXXX XXXX XXXX",
+"XXXXX           XXXX XXXX",
+"XXXXXXXXXXXXXX XXXXX XXXX",
+"XXXXXXXXXXXXXX XXXXX XXXX",
+"XXXX           XX    XXXX",
+"XXXX  XXXXXXXXXXXXXXXXXXX",
+"XXXX                    X",
+"XXXXXXXXXXXXXXXXXXXXXXXXX",
+]
+
+#Add maze to maze list
+levels.append(level_1)
+
+#Create Level Setup Function
+def setup_maze(level):
+    for y in range(len(level)):
+        for x in range (len(level[y])):
+            #get the character at each x,y coordinate
+            #note the order of y and x in the next line
+            character = level[y][x]
+            #calculate the screen x, y coordinates
+            screen_x = -288 + (x * 24)
+            screen_y = 288 - (y * 24)
+
+            #check if it is an X (representing a wall)
+            if character =="X":
+                pen.goto(screen_x, screen_y)
+                pen.stamp()
+
+#Create class instances
+pen = Pen()
+
+#Setting the level
+setup_maze(levels[1])
+
+#Main Game Loop
+while True:
+    pass
+
